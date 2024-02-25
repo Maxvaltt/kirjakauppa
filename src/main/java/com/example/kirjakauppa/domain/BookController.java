f83d040e986d282a18567882f1d5b0c8091fc914
Maxvaltt <maxmuino@gmail.com>
2024-02-25 19:56:30 +0200

    lisää ja poista toiminto

--- src/main/java/com/example/kirjakauppa/domain/BookController.java
+++ src/main/java/com/example/kirjakauppa/domain/BookController.java
@@ -3,6 +3,11 @@ package com.example.kirjakauppa.domain;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import io.micrometer.common.lang.NonNull;
 
 
 
@@ -19,8 +24,29 @@ public class BookController {
 
     @GetMapping("/booklist")
     public String showBookList(Model model) {
-        Iterable<book> books = bookRepository.findAll(); // Fetch all books from the database
-        model.addAttribute("books", books); // Add books to the model
-        return "booklist"; // Return the name of the Thymeleaf template
+        Iterable<book> books = bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "booklist";
     }
+
+    @GetMapping("/addbook")
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new book());
+        return "addbook";
     }
+
+    @SuppressWarnings("null")
+    @PostMapping("/addbook")
+    public String addBook(@ModelAttribute book newBook) {
+        bookRepository.save(newBook);
+        return "redirect:/booklist";
+    }
+
+
+
+    @PostMapping("/deletebook/{id}")
+public String deleteBook(@PathVariable @NonNull Long id) {
+    bookRepository.deleteById(id);
+    return "redirect:/booklist";
+}
+}

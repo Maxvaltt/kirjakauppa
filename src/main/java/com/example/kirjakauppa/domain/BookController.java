cfd94c03d65f28540a727d96e26ab1320a52302c
Maxvaltt <maxmuino@gmail.com>
2024-02-25 21:39:22 +0200

    editointi toiminto lisätty

--- src/main/java/com/example/kirjakauppa/domain/BookController.java
+++ src/main/java/com/example/kirjakauppa/domain/BookController.java
@@ -1,5 +1,7 @@
 package com.example.kirjakauppa.domain;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,11 +44,30 @@ public class BookController {
         return "redirect:/booklist";
     }
 
-
-
+    @SuppressWarnings("null")
     @PostMapping("/deletebook/{id}")
-public String deleteBook(@PathVariable @NonNull Long id) {
-    bookRepository.deleteById(id);
-    return "redirect:/booklist";
-}
+    public String deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
+    }
+
+    @SuppressWarnings("null")
+    @GetMapping("/editbook/{id}")
+    public String showEditBookForm(@PathVariable Long id, Model model) {
+        Optional<book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            model.addAttribute("book", optionalBook.get());
+            return "editbook";
+        } else {
+            return "redirect:/booklist";
+        }
+    }
+
+   
+    @PostMapping("/updatebook")
+    public String updateBook(@ModelAttribute @NonNull book updatedBook) {
+        bookRepository.save(updatedBook);
+        return "redirect:/booklist";
+    }
+    
 }
